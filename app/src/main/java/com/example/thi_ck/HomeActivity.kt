4b849ccf13f04a87ef16c1e45a15422ade45eabc
Duong <phamtanduongtk29@gmail.com>
2022-06-26 02:18:44 +0700

    update

--- app/src/main/java/com/example/thi_ck/HomeActivity.kt
+++ app/src/main/java/com/example/thi_ck/HomeActivity.kt
@@ -1,25 +1,24 @@
 package com.example.thi_ck
 
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.os.Bundle
-import android.os.Handler
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.ArraySet
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.thi_ck.Class.HomeItem
 import com.example.thi_ck.Handle.HandleAction
 import com.example.thi_ck.ItemAdapter.HomeAdapter
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
-import com.squareup.picasso.Picasso
-import java.util.concurrent.CompletableFuture
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var uid:String
@@ -37,20 +36,47 @@ class HomeActivity : AppCompatActivity() {
         uid = FirebaseAuth.getInstance().currentUser?.uid.toString()
         recyclerView = findViewById(R.id.home_recyclerview)
 
-        val dataset:ArrayList<HomeItem> = ArrayList()
-        dataset.add(HomeItem("WaIdUPPfXpTbuo8zxWWeuyRhl4U2","Duong ne","test home"))
-        dataset.add(HomeItem("THIZMaV0y6OOWvrvBtxba6yUwnL2","Anh","test home"))
-        dataset.add(HomeItem("xcp61oYQVggdd9hEdKj9TZ4z8Kx2","Nhi","test home"))
-        dataset.add(HomeItem("yuUhat923MRSpw8e8aQ5Axr2IJi1","Van","test home"))
-        adapter = HomeAdapter(dataset)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
+//        val dataset:ArrayList<HomeItem> = ArrayList()
+//        dataset.add(HomeItem("WaIdUPPfXpTbuo8zxWWeuyRhl4U2","Duong ne","test home"))
+//        dataset.add(HomeItem("THIZMaV0y6OOWvrvBtxba6yUwnL2","Anh","test home"))
+//        dataset.add(HomeItem("xcp61oYQVggdd9hEdKj9TZ4z8Kx2","Nhi","test home"))
+//        dataset.add(HomeItem("yuUhat923MRSpw8e8aQ5Axr2IJi1","Van","test home"))
+
+
 
     }
 
     override fun onStart() {
         super.onStart()
+        val dataset:ArrayList<HomeItem> = ArrayList()
+        val db = FirebaseFirestore.getInstance().collection("users")
+            db.get()
+                .addOnSuccessListener {
+                    for(i in it.documents) {
+                      if(!i["uid"].toString().equals(FirebaseAuth.getInstance()?.uid.toString())) {
+                          val qr = FirebaseFirestore.getInstance().collection("messages")
+                              .document(FirebaseAuth.getInstance()?.uid.toString()).collection(i["uid"].toString()).orderBy("date",Query.Direction.DESCENDING)
+                              .addSnapshotListener { snap, e ->
+                                  val data =snap?.documents
+                                    data?.sortByDescending{
+                                        val timestamp = it.get("date") as Timestamp
+                                        val milliseconds = timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000
+                                        val sdf = SimpleDateFormat("MM/dd/yyyy")
+                                        val netDate = Date(milliseconds)
+                                        val date = sdf.format(netDate)
+                                        netDate.time
+                                    }
+                                  if(data?.size != 0) {
+                                      dataset.add(HomeItem(i["uid"].toString(),i["username"].toString(),data?.get(0)?.get("message").toString()))
+                                      adapter = HomeAdapter(dataset)
+                                      recyclerView.layoutManager = LinearLayoutManager(this)
+                                      recyclerView.adapter = adapter
+                                  }
 
+                              }
+                      }
+                    }
+                }
 
     }
 
--- app/src/main/java/com/example/thi_ck/ItemAdapter/HomeAdapter.kt
+++ app/src/main/java/com/example/thi_ck/ItemAdapter/HomeAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.collection.ArraySet
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.thi_ck.ChatWithFriendActivity
@@ -14,42 +15,60 @@ import com.example.thi_ck.Class.User
 import com.example.thi_ck.R
 import com.squareup.picasso.Picasso
 
-class HomeAdapter(val dataset:ArrayList<HomeItem>):RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
+class HomeAdapter(val dataset: ArrayList<HomeItem>):RecyclerView.Adapter<HomeAdapter.ViewHolder>() {
 
     inner class ViewHolder(view:View):RecyclerView.ViewHolder(view) {
         lateinit var username:TextView
         lateinit var messages:TextView
         lateinit var image:ImageView
         lateinit var friend_uid:ConstraintLayout
+        lateinit var no_friend_chat:TextView
 
         init {
-            username = view.findViewById(R.id.username_friend)
-            messages = view.findViewById(R.id.mess_chat)
-            image = view.findViewById(R.id.image_friend)
-            friend_uid = view.findViewById(R.id.friend_uid)
+           if (dataset.size != 0) {
+               username = view.findViewById(R.id.username_friend)
+               messages = view.findViewById(R.id.mess_chat)
+               image = view.findViewById(R.id.image_friend)
+               friend_uid = view.findViewById(R.id.friend_uid)
+           }
+            else
+               no_friend_chat = view.findViewById(R.id.no_friend_chat)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_home_item, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(viewType, parent, false)
         return  ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val image_url = "https://media.istockphoto.com/vectors/default-avatar-man-to-social-user-vector-id860264876?k=6&m=860264876&s=170667a&w=0&h=2YApZVjoA2w1PBJonLA-PUfgbIHltDXqBZWlJI2Zo7M="
 
-        holder.username.setText(dataset.get(position).username)
-        holder.messages.setText(dataset.get(position).messages)
-        Picasso.get().load(image_url).into(holder.image)
+        if(dataset.size != 0) {
+            val image_url = "https://media.istockphoto.com/vectors/default-avatar-man-to-social-user-vector-id860264876?k=6&m=860264876&s=170667a&w=0&h=2YApZVjoA2w1PBJonLA-PUfgbIHltDXqBZWlJI2Zo7M="
 
-        holder.friend_uid.setOnClickListener {
-            val intent = Intent(holder.itemView.context,ChatWithFriendActivity::class.java)
-            val user = User(dataset.get(position).uid,dataset.get(position).username,image_url)
-            intent.putExtra("uid",user)
-            holder.itemView.context.startActivity(intent)
+            holder.username.setText(dataset.get(position).username)
+            holder.messages.setText(dataset.get(position).messages)
+            Picasso.get().load(image_url).into(holder.image)
+
+            holder.friend_uid.setOnClickListener {
+                val intent = Intent(holder.itemView.context,ChatWithFriendActivity::class.java)
+                val user = User(dataset.get(position).uid,dataset.get(position).username,image_url)
+                intent.putExtra("uid",user)
+                holder.itemView.context.startActivity(intent)
+            }
+        }
+        else {
+            holder.no_friend_chat.setText("Trống")
         }
 
 
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(dataset.size == 0) {
+            return R.layout.activity_no_friend_chat
+        }
+        return R.layout.activity_home_item
     }
 
     override fun getItemCount(): Int {
